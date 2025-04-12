d810291713c61d829e446eee4ca457e5d7b4d29f
Tomás Seco Cuenca <tomasseco2000@gmail.com>
2025-04-12 18:40:11 +0200

    capacitor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  // Firebase configuration
+  firebase: {
+    apiKey: "AIzaSyAjoOlgJGG9Mt5IJ8WUbO0fXzj_h3xe7EQ",
+    authDomain: "nbarookieapp-1592e.firebaseapp.com",
+    projectId: "nbarookieapp-1592e",
+    storageBucket: "nbarookieapp-1592e.firebasestorage.app",
+    messagingSenderId: "282892162098",
+    appId: "1:282892162098:web:bf9b0b4073cdb0cf13582b"
+  
+  }
 };
